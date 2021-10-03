ec1e2f7771db79a3cda7d019564d9ef0376d1d06
Emirhan Dogandemir <emirhandgndmr51@gmail.com>
2021-10-03 07:23:04 +0300

    logging added on PerformanceAspect

--- src/main/java/com/example/demo/aspects/performance/PerformanceLoggerAspect.java
+++ src/main/java/com/example/demo/aspects/performance/PerformanceLoggerAspect.java
@@ -3,7 +3,8 @@ package com.example.demo.aspects.performance;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
@@ -12,15 +13,16 @@ import org.springframework.stereotype.Component;
 @Order(1)
 public class PerformanceLoggerAspect {
 
+    private static final Logger logger =LoggerFactory.getLogger(PerformanceLoggerAspect.class);
+
     @Around("@annotation(com.example.demo.aspects.performance.PerformanceAspect)")
     public Object profile(ProceedingJoinPoint pjp) throws Throwable {
-        long start = System.currentTimeMillis();
-        System.out.println("Metot öncesi: " + pjp.getSignature());
+        long start = System.nanoTime();
+        logger.info("Metod başlatıldı",pjp.getSignature());
         Object output = pjp.proceed();
-        System.out.println("Metot sonrasi");
-        long elapsedTime = System.currentTimeMillis() - start;
-        System.out.println("Metot kosturma zamani: "
-                + elapsedTime + " milliseconds.");
+        logger.info("Metod bitirildi");
+        long elapsedTime = System.nanoTime() - start;
+        logger.info("Metot koşturulma süresi : ",elapsedTime);
         return output;
     }
 
